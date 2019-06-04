2b9c889e3673ba938d21b5391da81f9e5c026086
TinaDelli <cecurrier92@gmail.com>
2019-06-04 16:16:50 -0400

    forgot to commit, have the endpoints working for breeds ids and all dogs

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.java
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.java
@@ -1,5 +1,8 @@
-package com.lambdaschool.dogsinitial;
+package com.lambdaschool.dogsinitial.controller;
 
+import com.lambdaschool.dogsinitial.exception.ResourceNotFoundException;
+import com.lambdaschool.dogsinitial.model.Dog;
+import com.lambdaschool.dogsinitial.DogsinitialApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +17,24 @@ import java.util.ArrayList;
 public class DogController
 {
     // localhost:8080/dogs/dogs
-    @GetMapping(value = "/dogs")
+    @GetMapping(value = "/dogs", produces = {"application/json"})
     public ResponseEntity<?> getAllDogs()
     {
         return new ResponseEntity<>(DogsinitialApplication.ourDogList.dogList, HttpStatus.OK);
     }
 
     // localhost:8080/dogs/{id}
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}", produces = {"application/json"})
     public ResponseEntity<?> getDogDetail(@PathVariable long id)
     {
-        Dog rtnDog = DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id));
+        Dog rtnDog;
+        if (DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id)) == null)
+        {
+          throw new ResourceNotFoundException("Dog with id " + id + " not found");
+        } else
+        {
+            rtnDog = DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id));
+        }
         return new ResponseEntity<>(rtnDog, HttpStatus.OK);
     }
 
@@ -34,6 +44,10 @@ public class DogController
     {
         ArrayList<Dog> rtnDogs = DogsinitialApplication.ourDogList.
                 findDogs(d -> d.getBreed().toUpperCase().equals(breed.toUpperCase()));
+        if(rtnDogs.size() == 0)
+        {
+            throw new ResourceNotFoundException(breed + " is not a dog breed we have");
+        }
         return new ResponseEntity<>(rtnDogs, HttpStatus.OK);
     }
 }
