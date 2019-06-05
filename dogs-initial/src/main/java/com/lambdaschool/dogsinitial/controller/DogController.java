d3b97c4d2bb0e6a3372d87db8b6c9f22c59011b8
TinaDelli <cecurrier92@gmail.com>
2019-06-05 15:39:15 -0400

    added logging routines and created a rabitmq messaging system

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.java
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.java
@@ -3,6 +3,11 @@ package com.lambdaschool.dogsinitial.controller;
 import com.lambdaschool.dogsinitial.exception.ResourceNotFoundException;
 import com.lambdaschool.dogsinitial.model.Dog;
 import com.lambdaschool.dogsinitial.DogsinitialApplication;
+import com.lambdaschool.dogsinitial.model.MessageDetail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +22,20 @@ import java.util.ArrayList;
 @RequestMapping("/dogs")
 public class DogController
 {
+    private static final Logger logger = LoggerFactory.getLogger(DogController.class);
+
+    @Autowired
+    RabbitTemplate rt;
+
+
     // localhost:8080/dogs/dogs
     @GetMapping(value = "/dogs", produces = {"application/json"})
     public ResponseEntity<?> getAllDogs()
     {
+        logger.info("/dogs accessed");
+        MessageDetail message = new MessageDetail("/dogs accessed", 7, false);
+        rt.convertAndSend(DogsinitialApplication.QUEUE_NAME_HIGH, message);
+
         DogsinitialApplication.ourDogList.dogList.sort((d1, d2) -> (int)(d1.getBreed().compareToIgnoreCase(d2.getBreed())));
         return new ResponseEntity<>(DogsinitialApplication.ourDogList.dogList, HttpStatus.OK);
     }
@@ -29,6 +44,10 @@ public class DogController
     @GetMapping(value = "/{id}", produces = {"application/json"})
     public ResponseEntity<?> getDogDetail(@PathVariable long id)
     {
+        logger.info( "/dogs " + id + " accessed");
+        MessageDetail message = new MessageDetail( "dogs" + id + " accessed", 1, true);
+        rt.convertAndSend(DogsinitialApplication.QUEUE_NAME_LOW, message);
+
         Dog rtnDog;
         if (DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id)) == null)
         {
@@ -44,6 +63,10 @@ public class DogController
     @GetMapping(value = "/breeds/{breed}")
     public ResponseEntity<?> getDogBreeds (@PathVariable String breed)
     {
+        logger.info("/dogs/breeds " + breed + " accessed");
+        MessageDetail message = new MessageDetail("/dogs/breeds " + breed + " accessed", 2, true);
+        rt.convertAndSend(DogsinitialApplication.QUEUE_NAME_LOW, message);
+
         ArrayList<Dog> rtnDogs = DogsinitialApplication.ourDogList.
                 findDogs(d -> d.getBreed().toUpperCase().equals(breed.toUpperCase()));
         if(rtnDogs.size() == 0)
